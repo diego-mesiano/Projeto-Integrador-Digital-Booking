948da5ce6e264f60a071be4d62751f9aa4284fbe
Diego <dmesiano1@gmail.com>
2022-04-04 23:57:31 -0300

    Novas atualizações

--- digital-booking-fe/src/services/Api/index.jsx
+++ digital-booking-fe/src/services/Api/index.jsx
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const local = "http://localhost:8080";
-//const aws = "http://34.230.30.141:8080";
+//const local = "http://localhost:8080";
+const aws = "http://34.230.30.141:8080";
 
 const api = axios.create({
     
-    baseURL: local
+    baseURL: aws
     
 });
 
